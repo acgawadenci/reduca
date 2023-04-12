a511f2bc9c43ef9ea6c13ddd390cf567544feb9b
Achyut Gawade <gawade.achyut96@gmail.com>
2023-04-12 17:03:22 +0100

    AWS cred updated

--- src/main/java/com/acgawade/reduca/controller/DemoController.java
+++ src/main/java/com/acgawade/reduca/controller/DemoController.java
@@ -1,16 +1,10 @@
 package com.acgawade.reduca.controller;
 
-import com.acgawade.reduca.entity.Product;
 import com.acgawade.reduca.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.math.BigDecimal;
-import java.time.LocalDateTime;
-import java.util.List;
-import java.util.UUID;
 
 @RestController
 public class DemoController {
