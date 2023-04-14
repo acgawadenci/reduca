a3ebcb60295b8d8b4852ca160caf150afe1023e2
Achyut Gawade <gawade.achyut96@gmail.com>
2023-04-14 15:25:05 +0100

    Lazy fetch added

--- src/main/java/com/acgawade/reduca/security/User.java
+++ src/main/java/com/acgawade/reduca/security/User.java
@@ -1,10 +1,7 @@
 package com.acgawade.reduca.security;
 
 import jakarta.persistence.*;
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -17,6 +14,7 @@ import java.util.UUID;
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
+@ToString
 @Entity(name = "users")
 public class User implements UserDetails {
     @Id
@@ -34,7 +32,7 @@ public class User implements UserDetails {
     @Enumerated(EnumType.STRING)
     private Role role;
 
-    @OneToMany(mappedBy = "user", fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "user", fetch = FetchType.EAGER)
     private List<Token> tokens;
 
     @Override
