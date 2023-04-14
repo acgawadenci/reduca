22b7bae7aba775dbd321d2047e127cb67632848d
Achyut Gawade <gawade.achyut96@gmail.com>
2023-04-14 19:33:30 +0100

    Login API non secured

--- src/main/java/com/acgawade/reduca/controller/DemoController.java
+++ src/main/java/com/acgawade/reduca/controller/DemoController.java
@@ -1,13 +1,19 @@
 package com.acgawade.reduca.controller;
 
 import com.acgawade.reduca.repository.ProductRepository;
+import com.acgawade.reduca.repository.UserRepository;
+import com.acgawade.reduca.security.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class DemoController {
+    @Autowired
+    UserRepository userRepository;
     @Autowired
     ProductRepository productRepository;
 
@@ -19,5 +25,10 @@ public class DemoController {
     @GetMapping("/{userName}")
     public String demoResp(@PathVariable String userName) {
         return "This is also my demo Controller, Hello " + userName + " !!!";
+    }
+
+    @GetMapping("/getUser")
+    public List<User> getUsers() {
+        return userRepository.findAll();
     }
 }
