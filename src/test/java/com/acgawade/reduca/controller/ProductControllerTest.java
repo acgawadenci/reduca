a2552a1981d1f946197b79e37fc92e6cbcaf76e4
Achyut Gawade <gawade.achyut96@gmail.com>
2023-04-18 11:15:10 +0100

    AWS creds changed

---
+++ src/test/java/com/acgawade/reduca/controller/ProductControllerTest.java
@@ -0,0 +1,32 @@
+package com.acgawade.reduca.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductControllerTest {
+
+    @Test
+    void getProducts() {
+    }
+
+    @Test
+    void getListedProducts() {
+    }
+
+    @Test
+    void postProperty() {
+    }
+
+    @Test
+    void updateProperty() {
+    }
+
+    @Test
+    void deleteProperty() {
+    }
+
+    @Test
+    void contactOwner() {
+    }
+}
---
+++ src/test/java/com/acgawade/reduca/service/ProductServiceTest.java
@@ -0,0 +1,36 @@
+package com.acgawade.reduca.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ProductServiceTest {
+
+    @Test
+    void fetchProducts() {
+    }
+
+    @Test
+    void fetchMyProducts() {
+    }
+
+    @Test
+    void saveProduct() {
+    }
+
+    @Test
+    void updateProduct() {
+    }
+
+    @Test
+    void inactiveProduct() {
+    }
+
+    @Test
+    void updateImageUrl() {
+    }
+
+    @Test
+    void emailProductOwner() {
+    }
+}
